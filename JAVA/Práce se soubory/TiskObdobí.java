e398fb83f768c5deacde7536c98da5cf037fbc55
KrivSot <56109982+KrivSot@users.noreply.github.com>
2019-10-15 10:49:26 +0200

    java

---
+++ JAVA/Práce se soubory/TiskObdobí.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.awt.Color;
+public class TiskObdobí
+{
+    public static void main(String[] args)
+    {
+        for(Období obd : Období.values())
+        {
+            System.out.println(obd+"   "+obd.ordinal()+" "+obd.getParametry());
+            
+            EnumSet<Období> set0 = EnumSet.allOf(Období.class);
+            Sys
+        }
+    }
+}
